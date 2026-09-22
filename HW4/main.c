cb51584a34bb5f8acc1d2414c3e74407186647c7
Daniil Kozliuk <dank0w01ff@gmail.com>
2026-09-22 09:44:17 +0000

    HW4 added

---
+++ HW4/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+	int a = 5;
+	printf("%d squared is %d\n", a, square(a));
+	return 0;
+}
---
+++ HW4/math_utils.h
@@ -0,0 +1,8 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+/*
+ * Returns a square of the integer 'a'
+ */
+int square(int a);
+
+#endif
